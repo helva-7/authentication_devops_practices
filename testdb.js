2dabf6f672e05fb1c3212bcb9a187cee9a95f37f
helva-7 <helva71412@gmail.com>
2025-08-14 09:48:41 +0000

    se

--- testdb.js
+++ testdb.js
@@ -1,3 +1,23 @@
 require('dotenv').config();
+const mysql= require('mysql2/promise');
 
-console.log('DB_PASSWORD is:', process.env.DB_PASS);
+async function testDbConnection(){
+    try{
+        const connection = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+            port: process.env.DB_PORT,
+        });
+        console.log('dakchi mzyan');
+        await connection.end();
+        process.exit(0);
+    }
+    catch(err){
+        console.error('database connection failed:', err);
+        process.exit(1);
+    }
+}
+
+testDbConnection();
